d12b588ac430e82171137b5a471f83d5e322f456
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-10-07 13:19:28 +0200

    first file

---
+++ alien_invasion.py
@@ -0,0 +1,7 @@
+import sys
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour"""
+
+    def __init__(self):
+        """Initialise the game, and creat game resources."""
